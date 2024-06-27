6f18bbec49b4b7feee76ad64f3674c6bede27822
Rinae Hyun <rnhyun.dream@gmail.com>
2024-06-27 15:55:20 +0200

    Practice Array tasks with getting sum and max

---
+++ src/main/java/ArrayTask.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class ArrayTask {
+    public static void main(String[] args) {
+
+
+        int[] firstArray = new int[10];
+
+        // Step 1: Create an array with 1, ..., 10
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = i + 1;
+        }
+        System.out.println("The first array is: " + Arrays.toString(firstArray));
+
+        //int[] firstArray = {3, 6, 8, 9, 10, 1, 4, 2, 7, 5};
+
+        // Step 2: Get the sum of all elements in the array
+        int sumAllElements = 0;
+        for (int j : firstArray) {
+            sumAllElements += j;
+        }
+        System.out.println("The sum of all elements in the first array is: " + sumAllElements);
+
+
+        // Step 3: Get the max element in the array (with for loop)
+        int maxElement = firstArray[0];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            if (maxElement < firstArray[i]) {
+                maxElement = firstArray[i];
+            }
+        }
+        System.out.println("The max element in the first array (for loop) is: " + maxElement);
+
+
+        // Step 3: Get the max element in the array (with sort method)
+        /* if .sort() used, the original Array has been changed */
+        Arrays.sort(firstArray);
+        int maxElementWithArray = firstArray[firstArray.length - 1];
+        System.out.println("The max element in the first array (sort method) is: " + maxElementWithArray);
+        //System.out.println(Arrays.toString(firstArray));
+    }
+}
