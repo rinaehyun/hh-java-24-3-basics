f62d2c19cef6aec341357d98b57d32fb022c0170
Rinae Hyun <rnhyun.dream@gmail.com>
2024-06-28 13:56:51 +0200

    Update the file name and add comments for the method

--- src/main/java/grouptasks/JUnitTask.java
+++ src/main/java/grouptasks/JUnitTask.java
@@ -3,14 +3,22 @@ package grouptasks;
 import java.util.Arrays;
 import java.util.List;
 
-public class TDDTask {
+public class JUnitTask {
     public static void main(String[] args) {
 
+        // Bonus task
         String greetings = "hey, hola!";
         boolean lanCheck = validateHello(greetings);
         System.out.println(lanCheck);
     }
 
+
+    // ** From codewars ** //
+    /**
+     * Function name: validateHello
+     * @param greetings (String)
+     * @return a choice (String).
+     */
     public static boolean validateHello(String greetings){
 
         String[] greetingsArray = {"hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"};
