4b3e1328278bc9376d7692e36f627ea410420423
Rinae Hyun <rnhyun.dream@gmail.com>
2024-06-28 14:27:19 +0200

    Use valueOf() method to convert number to String

--- src/main/java/tutorials/TDDExamples.java
+++ src/main/java/tutorials/TDDExamples.java
@@ -3,14 +3,6 @@ package tutorials;
 public class TDDExamples {
 
     public static String frizzBuzz(int number) {
-        if (number == 7) {
-            return "7";
-        }
-
-        if (number == 1) {
-            return "1";
-        }
-
-        return null;
+        return String.valueOf(number);
     }
 }
