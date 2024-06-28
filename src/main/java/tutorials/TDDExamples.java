80aa5233dfbd600fc943f125bcfa8ba9c76898f5
Rinae Hyun <rnhyun.dream@gmail.com>
2024-06-28 21:06:59 +0200

    Add further tests to check if the number is divided by 3, 5 and 15

--- src/main/java/tutorials/TDDExamples.java
+++ src/main/java/tutorials/TDDExamples.java
@@ -2,7 +2,16 @@ package tutorials;
 
 public class TDDExamples {
 
-    public static String frizzBuzz(int number) {
+    public static String fizzBuzz(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/tutorials/TDDExamplesTest.java
+++ src/test/java/tutorials/TDDExamplesTest.java
@@ -3,17 +3,17 @@ package tutorials;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static tutorials.TDDExamples.frizzBuzz;
+import static tutorials.TDDExamples.fizzBuzz;
 
 class TDDExamplesTest {
 
     @Test
-    public void frizzBuzzTest_whenNumber1_thenReturn1AsString() {
+    public void fizzBuzzTest_whenNumber1_thenReturn1AsString() {
         // GIVEN
         int num = 1;
 
         // WHEN
-        String actual = frizzBuzz(num);
+        String actual = fizzBuzz(num);
 
         // THEN
         assertEquals("1", actual);
@@ -21,14 +21,53 @@ class TDDExamplesTest {
 
 
     @Test
-    public void frizzBuzzTest_whenNumber7_thenReturn7AsString() {
+    public void fizzBuzzTest_whenNumber7_thenReturn7AsString() {
         // GIVEN
         int num = 7;
 
         // WHEN
-        String actual = frizzBuzz(num);
+        String actual = fizzBuzz(num);
 
         // THEN
         assertEquals("7", actual);
+    }
+
+
+    @Test
+    public void frizzBuzzTest_whenNumber3_thenReturnFizz() {
+        // GIVEN
+        int num = 3;
+
+        // WHEN
+        String actual = fizzBuzz(num);
+
+        // THEN
+        assertEquals("Fizz", actual);
+    }
+
+
+    @Test
+    public void frizzBuzzTest_whenNumber5_thenReturnBuzz() {
+        // GIVEN
+        int num = 5;
+
+        // WHEN
+        String actual = fizzBuzz(num);
+
+        // THEN
+        assertEquals("Buzz", actual);
+    }
+
+
+    @Test
+    public void frizzBuzzTest_whenNumber15_thenReturnFizzBuzz() {
+        // GIVEN
+        int num = 15;
+
+        // WHEN
+        String actual = fizzBuzz(num);
+
+        // THEN
+        assertEquals("FizzBuzz", actual);
     }
 }
