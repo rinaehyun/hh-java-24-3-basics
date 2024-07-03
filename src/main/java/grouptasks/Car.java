16e027f9d122d4b9c665793fcb788b3b6211a4c7
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-03 11:26:04 +0200

    Add constructors and create objects in different ways

--- src/main/java/grouptasks/Car.java
+++ src/main/java/grouptasks/Car.java
@@ -1,12 +1,31 @@
 package grouptasks;
 
 public class Car {
+    // Fields
     String brand;
     String model;
     String color;
     int yearOfManufacture;
     double speed;
 
+
+    // Constructors
+    public Car() {
+    }
+
+    public Car(double speed) {
+        this.speed = speed;
+    }
+
+    public Car(String brand, String model, String color, int yearOfManufacture, double speed) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.yearOfManufacture = yearOfManufacture;
+        this.speed = speed;
+    }
+
+    // Methods
     public void starts() {
         System.out.println("The car starts to drive.");
     }
--- src/main/java/grouptasks/OOPExamples.java
+++ src/main/java/grouptasks/OOPExamples.java
@@ -3,13 +3,33 @@ package grouptasks;
 public class OOPExamples {
     public static void main(String[] args) {
 
-        // the first Car object
+        // the first Car object -> without any fields
         Car myCar = new Car();
         myCar.starts();
 
-        // the second Car object
-        Car yourCar = new Car();
+        System.out.println(myCar.brand);
+        System.out.println(myCar.model);
+        System.out.println(myCar.color);
+        System.out.println(myCar.yearOfManufacture);
+
+        // the second Car object -> with one field
+        Car yourCar = new Car(80.0);
+        System.out.println(yourCar.speed);
+
         yourCar.accelerates(125.5);
         System.out.println(yourCar.speed);
+
+        System.out.println(myCar.brand);
+        System.out.println(myCar.model);
+        System.out.println(myCar.color);
+        System.out.println(myCar.yearOfManufacture);
+
+        // the third Car object -> with all fields
+        Car ourCar = new Car("BMW", "BMW i7", "Black", 2020, 180.5);
+        System.out.println(ourCar.brand);
+        System.out.println(ourCar.model);
+        System.out.println(ourCar.color);
+        System.out.println(ourCar.yearOfManufacture);
+        System.out.println(ourCar.speed);
     }
 }
