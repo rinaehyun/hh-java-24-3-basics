29de05a93b6ba0e49974a4822cabfa0841dace2d
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-03 11:07:24 +0200

    Create an example of oop with class car

---
+++ src/main/java/grouptasks/Car.java
@@ -0,0 +1,12 @@
+package grouptasks;
+
+public class Car {
+    String brand;
+    String model;
+    String color;
+    int yearOfManufacture;
+
+    public void starts() {
+        System.out.println("The car starts to drive.");
+    }
+}
---
+++ src/main/java/grouptasks/OOPExamples.java
@@ -0,0 +1,9 @@
+package grouptasks;
+
+public class OOPExamples {
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+        myCar.starts();
+    }
+}
