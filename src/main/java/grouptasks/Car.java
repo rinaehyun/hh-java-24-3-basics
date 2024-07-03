de0e1dc7dd37012f1dab6392c9da1c28c3139f10
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-03 11:13:00 +0200

    Extend the class with accelerate method and create an object

--- src/main/java/grouptasks/Car.java
+++ src/main/java/grouptasks/Car.java
@@ -5,8 +5,13 @@ public class Car {
     String model;
     String color;
     int yearOfManufacture;
+    double speed;
 
     public void starts() {
         System.out.println("The car starts to drive.");
+    }
+
+    public void accelerates(double speed) {
+        this.speed = speed;
     }
 }
--- src/main/java/grouptasks/OOPExamples.java
+++ src/main/java/grouptasks/OOPExamples.java
@@ -3,7 +3,13 @@ package grouptasks;
 public class OOPExamples {
     public static void main(String[] args) {
 
+        // the first Car object
         Car myCar = new Car();
         myCar.starts();
+
+        // the second Car object
+        Car yourCar = new Car();
+        yourCar.accelerates(125.5);
+        System.out.println(yourCar.speed);
     }
 }
