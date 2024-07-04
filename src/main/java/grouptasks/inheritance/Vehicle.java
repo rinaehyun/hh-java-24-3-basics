d3a6b0df625e90fbd866fb1b330d195aa9b92b91
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-04 14:30:02 +0200

    Create parent- and child-class

---
+++ src/main/java/grouptasks/inheritance/Vehicle.java
@@ -0,0 +1,57 @@
+package grouptasks.inheritance;
+
+import java.util.Objects;
+
+public class Vehicle {
+    // Fields
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+
+    // Constructors
+    public Vehicle() {
+    }
+
+    public Vehicle(String manufacturer, String model, int yearOfManufacture) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    // Getters and Setters
+    public String getManufacturer() { return manufacturer; }
+
+    public String getModel() { return model; }
+
+    public int getYearOfManufacture() { return yearOfManufacture; }
+
+    public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
+
+    public void setModel(String model) { this.model = model; }
+
+    public void setYearOfManufacture(int yearOfManufacture) { this.yearOfManufacture = yearOfManufacture; }
+
+    // Comparisons
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return yearOfManufacture == vehicle.yearOfManufacture && Objects.equals(manufacturer, vehicle.manufacturer) && Objects.equals(model, vehicle.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(manufacturer, model, yearOfManufacture);
+    }
+
+    // Print
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", yearOfManufacture=" + yearOfManufacture +
+                '}';
+    }
+}
