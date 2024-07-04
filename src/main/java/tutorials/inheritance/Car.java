207818a7d354966777094ff7f3e6abf78ef0a1db
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-04 12:59:03 +0200

    Add note regarding inheritance from the lecture

---
+++ src/main/java/tutorials/inheritance/Car.java
@@ -0,0 +1,87 @@
+package tutorials.inheritance;
+
+import java.util.Objects;
+
+public class Car extends Vehicle {
+    // Fields
+    private int length;
+    private int numberOfDoors;
+    private int numberOfWheels;
+
+
+    // Constructors
+    public Car() {
+        super();
+    }
+
+    public Car(int length, int numberOfDoors, int numberOfWheels) {
+        super();
+        this.length = length;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public Car(String brand, String model, int numberOfDoors, int numberOfWheels) {
+        super(brand, model);
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public Car(String brand, String model, String color, String type, int length, int numberOfDoors, int numberOfWheels) {
+        super(brand, model, color, type);
+        this.length = length;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    // Getters and setters
+    public int getLength() {
+        return length;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    // Comparison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Car car = (Car) o;
+        return length == car.length && numberOfDoors == car.numberOfDoors && numberOfWheels == car.numberOfWheels;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), length, numberOfDoors, numberOfWheels);
+    }
+
+    // Print
+    @Override
+    public String toString() {
+        return "Car{" +
+                "length=" + length +
+                ", numberOfDoors=" + numberOfDoors +
+                ", numberOfWheels=" + numberOfWheels +
+                '}';
+    }
+
+}
---
+++ src/main/java/tutorials/inheritance/Main.java
@@ -0,0 +1,40 @@
+package tutorials.inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Compare the result of methods -> The children has a method, which overrides the one from the parent.
+        compareMethodsFromParentAndChild();
+
+        // Initialize Car objects with different constructors
+        compareConstructors();
+    }
+
+
+    public static void compareMethodsFromParentAndChild() {
+        Bike myBike = new Bike();
+        myBike.drive();
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.drive();
+    }
+
+    public static void compareConstructors() {
+        Car carWithoutFields = new Car();
+        System.out.println(carWithoutFields);
+
+        Car carWithChildrenFields = new Car(50, 4, 4);
+        System.out.println(carWithChildrenFields);
+
+        Car carWithPartOfFields = new Car("BMW", "X1", 5, 4);
+        System.out.println(carWithPartOfFields);
+
+        Car carWithParentFields = new Car("Audi", "A6", "white", "Combi", 300, 5, 4);
+        System.out.println(carWithParentFields);
+
+        Bike bikeOnlyWithParentFields = new Bike("Giant", "bike1");
+        System.out.println(bikeOnlyWithParentFields);
+    }
+
+}
