2163d175f7418eabd4310cf553fe0559df2a85de
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-11 14:50:05 +0200

    Use Optional to avoid nullPointerException

--- src/main/java/grouptasks/enums/Main.java
+++ src/main/java/grouptasks/enums/Main.java
@@ -1,6 +1,7 @@
 package grouptasks.enums;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,8 +16,31 @@ public class Main {
         );
 
         PersonRepository personRepo = new PersonRepository(personList);
-        Person person = personRepo.findPersonById("A2");
+        Optional<Person> person = personRepo.findPersonById("A1");
 
+        // Check when we return values as Optional
+        checkReturnAsOptional(person);
+
+        // Show details of a filtered class
+        showNameAndDayFoundById(person);
+    }
+
+    public static void checkReturnAsOptional(Optional<Person> person) {
         System.out.println(person);
+        System.out.println("The Optional is empty?: " + person.isEmpty());
+        System.out.println("There is a non-null value inside the Optional object?: " + person.isPresent());
+
+    }
+
+    public static void showNameAndDayFoundById(Optional<Person> person) {
+        person.ifPresent(personSelected -> {
+            String id = personSelected.id();
+            String name = personSelected.name();
+            DaysOfWeek favDay = personSelected.favoriteDay();
+
+            String dayCapitalized = favDay.toString().charAt(0) + favDay.toString().substring(1).toLowerCase();
+
+            System.out.println("The person's id is " + id + ", name is " + name + ", favoriate day is " + dayCapitalized + ".");
+        });
     }
 }
--- src/main/java/grouptasks/enums/PersonRepository.java
+++ src/main/java/grouptasks/enums/PersonRepository.java
@@ -3,6 +3,7 @@ package grouptasks.enums;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class PersonRepository {
     private List<Person> personRepo = new ArrayList<>();
@@ -11,12 +12,12 @@ public class PersonRepository {
         this.personRepo = personRepo;
     }
 
-    public Person findPersonById(String id) {
+    public Optional<Person> findPersonById(String id) {
         for (Person person : personRepo) {
             if (Objects.equals(person.id(), id)) {
-                return person;
+                return Optional.of(person);
             }
         }
-        return null;
+        return Optional.empty();
     }
 }
