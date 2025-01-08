1994abdcd754108dcaa8741bb4fb61ce3663a7bf
Rinae Hyun <rnhyun.dream@gmail.com>
2025-01-08 14:51:47 +0100

    Add examples of add two arrays and print each letter of a string

--- src/main/java/review/ArraysExample.java
+++ src/main/java/review/ArraysExample.java
@@ -1,11 +1,13 @@
 package review;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraysExample {
     public static void main(String[] args) {
 
         int[] numbers = {3, 2, 4, 7, 5};
+        int[] numbers2 = {5, 1, 6, 9, 8};
 
         System.out.println("=== For Loop ===");
         showForLoop(numbers);
@@ -29,6 +31,12 @@ public class ArraysExample {
 
         System.out.println("=== the largest number ===");
         findLargestNumber(numbers);
+
+        System.out.println("=== Add two arrays ===");
+        addTwoArrays(numbers, numbers2);
+
+        System.out.println("=== Read letters ===");
+        readLetters();
     }
 
     public static void showForLoop(int[] numbers) {
@@ -63,5 +71,26 @@ public class ArraysExample {
             }
         }
         System.out.println(max);
+    }
+
+    public static void addTwoArrays(int[] numbers1, int[] numbers2) {
+       if (numbers1.length == numbers2.length) {
+            int[] sum = new int[numbers1.length];
+            for (int i = 0; i < numbers1.length; i++) {
+                sum[i] = numbers1[i] + numbers2[i];
+            }
+            System.out.println(Arrays.toString(sum));
+       }
+    }
+
+    public static void readLetters() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type any words: ");
+        String str = scanner.nextLine();
+        scanner.close();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
     }
 }
