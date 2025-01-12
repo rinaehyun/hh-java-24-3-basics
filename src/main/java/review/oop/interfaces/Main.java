046c25974ec97bd3b7e3d5debb4f084e381b25ce
Rinae Hyun <rnhyun.dream@gmail.com>
2025-01-12 13:24:56 +0100

    Add examples of interfaces and using Object as parameter

--- src/main/java/review/oop/interfaces/Main.java
+++ src/main/java/review/oop/interfaces/Main.java
@@ -2,10 +2,19 @@ package review.oop.interfaces;
 
 public class Main {
     public static void main(String[] args) {
+        // Example from tutorial
         Shape circle = new Circle(3.5);
         Shape square = new Square(3);
 
         System.out.println(circle.calculateArea());
         System.out.println(square.calculateArea());
+
+        // Example from challenges
+        Playable musicPlayer = new MusicPlayer();
+        Playable videoPlayer = new VideoPlayer();
+
+        MediaController mediaController = new MediaController();
+        mediaController.playMedia(musicPlayer);
+        mediaController.playMedia(videoPlayer);
     }
 }
---
+++ src/main/java/review/oop/interfaces/Playable.java
@@ -0,0 +1,5 @@
+package review.oop.interfaces;
+
+public interface Playable {
+    void play();
+}
